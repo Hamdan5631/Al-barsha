229c3f3b71764aa67d3d6b444632ac8ba63b22bb
mohammedhamdan <mohammedhamdank634@gmail.com>
2026-03-31 19:47:49 +0530

    Update validation rule for company stamp in UpdateSettingsRequest
    - Changed the validation key from 'company_stamp' to 'company_stamp_url' to align with recent updates in the SettingsController.

--- app/Http/Requests/Settings/UpdateSettingsRequest.php
+++ app/Http/Requests/Settings/UpdateSettingsRequest.php
@@ -18,7 +18,7 @@ class UpdateSettingsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'company_stamp' => ['sometimes', 'file', 'image', 'max:4096'],
+            'company_stamp_url' => ['sometimes', 'file', 'image', 'max:4096'],
             'remove_company_stamp' => ['sometimes', 'boolean'],
             'invoice_company_name' => ['sometimes', 'nullable', 'string', 'max:255'],
             'invoice_footer_line1' => ['sometimes', 'nullable', 'string', 'max:600'],
